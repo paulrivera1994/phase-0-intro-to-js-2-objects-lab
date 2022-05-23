cac75b701665f0090de039744658a5462d93af3e
Paul Rivera <juniorrivera81@yahoo.com>
2022-05-23 11:54:43 -0600

    commit tree 9c755bb35ff7bd272fabe139df89c4368ac0746f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = {
+    name : 'Jorge',
+    streetAddress : '10th street'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee,[key]: value}  
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    return {...employee[key]}
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
